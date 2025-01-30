1b360fd46975c370e100c744423ac224c7d030ce
Amulya20062007 <padamatiamulya@gmail.com>
2025-01-30 05:37:08 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int num1=5;
+        int num2=5;
+        int result=num1*num2;
+        System.err.println("the result of Multiplication is :"+ result);
+    }
+}
